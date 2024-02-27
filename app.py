e2cf542ff2eff1ced0d5c1772f3ef98f95f6359d
igorDeveloper <138522037+igor-gdf@users.noreply.github.com>
2024-02-27 13:46:27 +0000

    primeiro commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/editpassword')
+def edit():
+    return render_template('editpassword.html')
